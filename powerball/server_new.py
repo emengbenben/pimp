55e90a0ee12df906eb23b36b4468ffbc87ceb1e5
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-29 00:56:09 -0400

    Update server_new.py

--- powerball/server_new.py
+++ powerball/server_new.py
@@ -1,6 +1,6 @@
 from Homepage import Homepage 
 
-from packets import RequestAdmission, ProofOfPayment, PaymentResult
+from packets import RequestTransfer, RequestAdmission, ProofOfPayment, PaymentResult
 from packets import RequestGame, GameRequest, GameResponse
 
 import asyncio, os, pickle
@@ -14,11 +14,9 @@ from playground.network.packet import PacketType
 
 class PaymentProcessing:
     def __init__(self):
-        self._bankconfig = OnlineBankConfig()
-        
+        self._bankconfig = OnlineBankConfig()       
         # TODO: This is hard coded. It needs to be changed to be part of the ini
         certPath = os.path.join(self._bankconfig.path(), "bank.cert")
-        
         
         self._cert = loadCertFromFile(certPath)
         self._account = None
@@ -37,7 +35,7 @@ class PaymentProcessing:
         req_admission = RequestAdmission(
             account=self._account,
             amount =self._price,
-            token  =token
+            token  = token
         )
         self._tokens[ token ] = "WAITING"
         return req_admission
@@ -68,11 +66,48 @@ class PaymentProcessing:
                 return "Signature failed"
             return self._verifyReceipt(receipt, token)
         return "Unknown Token"
+
+    def set_src_account(self, src_account):
+        """This is not async. should be called before loop starts or in executor"""
+        self._login_name = input("Enter bank login name for account {}: ".format(src_account))
+        self._password   = getpass.getpass("Password: ")
+        self._src_account = src_account
+
+    async def make_payment(self, dst_account, amount, memo):
+        loop = asyncio.get_event_loop()
+        bank_addr = self._bankconfig.get_parameter("CLIENT","bank_addr")
+        bank_port = int(self._bankconfig.get_parameter("CLIENT","bank_port"))
+
+        print("Connect to bank {}:{} for payment.".format(bank_addr, bank_port))
+
+        transport, protocol = await playground.create_connection(
+            lambda: BankClientProtocol(self._cert, self._login_name, self._password),
+            bank_addr,
+            bank_port)
+        try:
+            result = await protocol.loginToServer()
+        except Exception as e:
+            print("Could not log in because", e)
+            self.transport.close()
+            return None
+        try:
+            result = await protocol.switchAccount(self._src_account)
+            result = await protocol.transfer(dst_account, amount, memo)
+        except Exception as e:
+            result = None
+            print("Could not transfer funds because", e)
+        try:
+            protocol.close()
+        except Exception as e:
+            print ("Warning, could not close bank connection because", e)
+        return result
+
 global_payment_processor = PaymentProcessing()
 
 class HomepageServerProtocol(asyncio.Protocol):
     def __init__(self):
         self._buffer = PacketType.Deserializer()
+        self._token = None
         
     def connection_made(self, transport):
         print("Server connected")
@@ -81,9 +116,23 @@ class HomepageServerProtocol(asyncio.Protocol):
     def data_received(self, data):
         self._buffer.update(data)
         for packet in self._buffer.nextPackets():
+            print("Server got", packet)
             if isinstance(packet, RequestGame):
                 req = global_payment_processor.createAdmissionRequest()
                 self.transport.write(req.__serialize__())
+
+            elif isinstance(packet, RequestAdmission):
+                if self._token != None:
+                    self.transport.close()
+                    raise Exception("Already paid!")
+                else:
+                    self._token = packet.token
+                    make_payment_coro = self.pay_for_admission(
+                        packet.account,
+                        packet.amount,
+                        packet.token)
+                    asyncio.ensure_future(make_payment_coro)
+
             elif isinstance(packet, ProofOfPayment):
                 payment_status = global_payment_processor.process(
                     packet.token,
@@ -108,6 +157,15 @@ class HomepageServerProtocol(asyncio.Protocol):
                         message= payment_status)
                     self.transport.write(response.__serialize__())
                     self.transport.close()
+
+            elif isinstance(packet, PaymentResult):
+                if not packet.accepted:
+                    print("Payment rejected: ", packet.accepted)
+                    self.transport.close()
+                else:
+                    print("Payment accepted.")
+                    self.transport.close()
+
             elif isinstance(packet, GameRequest):
                 if packet.token != self._token:
                     self.transport.close()
@@ -124,7 +182,27 @@ class HomepageServerProtocol(asyncio.Protocol):
                         status  =status)
                     self.transport.write(game_response.__serialize__())
                     if status == "6":
-                        self.transport.close()
+                        request_transfer = RequestTransfer(
+                            amount = self.homepage.getcurrency()
+                        )
+                        self.transport.write(request_transfer.__serialize__())
+                        print("request transfer sent")
+                        #self.transport.close()
+
+    async def pay_for_admission(self, dst_account, amount, token):
+        
+        result = await global_payment_processor.make_payment(dst_account, amount, token)
+        if result == None:
+            self.transport.close()
+            return False
+        
+        proof = ProofOfPayment(
+            token    = self._token,
+            receipt  = result.Receipt, 
+            signature= result.ReceiptSignature)
+        
+        self.transport.write(proof.__serialize__())
+        return True
               
 
 
@@ -140,10 +218,12 @@ if __name__=="__main__":
     
     args = parser.parse_args(sys.argv[1:])
     global_payment_processor.configure(args.account, int(args.price))
+    global_payment_processor.set_src_account(args.account)
     
     loop = asyncio.get_event_loop()
     coro = playground.create_server(HomepageServerProtocol, host='20191.2.10.1', port=args.port)
     server = loop.run_until_complete(coro)
+
     
     # Serve requests until Ctrl+C is pressed
     print('Serving on {}'.format(server.sockets[0].getsockname()))
