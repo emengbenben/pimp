53b12f36c1ea9d029a90c9908e34b864464af58d
Benfang Wang <wangbenfang2016@outlook.com>
2019-05-01 19:42:53 -0400

    Update server_new.py

--- powerball/server_new.py
+++ powerball/server_new.py
@@ -242,12 +242,11 @@ if __name__=="__main__":
     parser.add_argument("-p", "--port", default=5657)
     parser.add_argument("--price", default = 0)
     parser.add_argument("-s", "--stack", default = "pimp")
-
-    stack = args.stack
     
     args = parser.parse_args(sys.argv[1:])
     global_payment_processor.configure(args.account, int(args.price))
     global_payment_processor.set_src_account(args.account)
+    stack = args.stack
     
     loop = asyncio.get_event_loop()
     coro = playground.create_server(HomepageServerProtocol, host='20191.2.10.1', port=args.port,family = stack)
